6201f6364a3cf3b86b83f7a147f5056ef9f5c5f9
ghaid999 <gaid.909@gmail.com>
2026-08-29 21:58:03 +0300

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""task 0 add two vairible a and b and return a float"""
+"""task 0 add """
 
 
 def add(a: float, b: float) -> float:
+     """ two vairible a and b and return a float"""
     c = a + b
     return c
