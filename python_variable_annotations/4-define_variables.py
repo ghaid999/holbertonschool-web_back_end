1b403cf9dbf99785c3be4018e4401389d8b699e5
ghaid999 <gaid.909@gmail.com>
2026-08-29 22:14:18 +0300

    Create  4-define_variables.py

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" task 4 """
+
+
+""" Define and annotate the following variables with the specified values"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: boolean = True
+school: str =  "Holberton"
