3a1687b39ec71ebf736271c56288b84f708167a6
ghaid999 <gaid.909@gmail.com>
2026-08-29 22:15:42 +0300

    Update 4-define_variables.py

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -5,5 +5,5 @@
 """ Define and annotate the following variables with the specified values"""
 a: int = 1
 pi: float = 3.14
-i_understand_annotations: boolean = True
+i_understand_annotations: bool= True
 school: str =  "Holberton"
