9fd944df002779cca553ccc12173c0c0c00e9f84
ghaid999 <gaid.909@gmail.com>
2026-08-29 22:23:29 +0300

    Fix type hint for sum_mixed_list function

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-""" task 6 """
-from typing import List
+"""Task 6"""
+
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
-  """  which takes a list mxd_lst of integers and floats and returns their sum as a float"""
-  return sum(mxd_lst)
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Return the sum of a list of integers and floats as a float."""
+    return sum(mxd_lst)
