db7ce731b70e60800445eeb4ed3d378007e4b3cf
ghaid999 <gaid.909@gmail.com>
2026-09-12 23:56:13 +0300

    Create 12-log_stats.py

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Provide statistics about Nginx logs."""
+
+from pymongo import MongoClient
+
+
+def main():
+    """Print Nginx log statistics."""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collection = client["logs"]["nginx"]
+
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = collection.count_documents({"method": method})
+        print("\t{}: {}".format(method, count))
+
+    status = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print("{} status check".format(status))
+
+
+if __name__ == "__main__":
+    main()
