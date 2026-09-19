af924011ca7ef7bf7e4daf6fd876a654b4440a28
ghaid999 <gaid.909@gmail.com>
2026-09-19 23:30:49 +0300

    Update 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,15 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-
-    if (this.constructor.prototype.evacuationWarningMessage === undefined) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
   }
 }
