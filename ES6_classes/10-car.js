1f49dc5943f119c0417722cb9a4759d274b0a76b
ghaid999 <gaid.909@gmail.com>
2026-09-19 23:41:09 +0300

    Create 10-car.js

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,21 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const CarClass = this.constructor[Symbol.species];
+
+    return new CarClass(
+      this._brand,
+      this._motor,
+      this._color,
+    );
+  }
+}
