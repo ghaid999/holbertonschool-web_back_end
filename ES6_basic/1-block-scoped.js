0f0e649c385ea6d3337b6be4be0547cdf0e2e237
ghaid999 <gaid.909@gmail.com>
2026-09-19 22:53:09 +0300

    Update 1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    const taskBlock = true;
+    const task2Block = false;
   }
 
   return [task, task2];
